0048748110299a3ed61c6a26e876a20303664306
Ivan Kriventsev <81254599+xD1rty@users.noreply.github.com>
2023-07-23 11:06:50 +0300

    Delete nvim directory

--- nvim/lua/core/mappings.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = ' '
-
-vim.keymap.set('n', '<Leader>qq', ':NvimTreeFocus<CR>')
-vim.keymap.set('n', '<Leader>nn', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', 'gT', '<Cmd>BufferPrevious<CR>')
-vim.keymap.set('n', 'gt', '<Cmd>BufferNext<CR>')
--- nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "python", "go" },
-
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-}
